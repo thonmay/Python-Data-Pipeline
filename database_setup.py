d6291b3d2bc52e44975fc3896bd53884a199cd60
Thonmay <mdthoriqulislam384@gmail.com>
2025-08-16 18:55:40 +0200

    Database Setup: set up the database using sqlite3

---
+++ database_setup.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+DB_FILE = "image_metadata.bd"
+
+conn = sqlite3.connect(DB_FILE)
+
+cursor = conn.cursor()      # for performing SQL commands
+
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS metadat (
+    image_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    file_name TEXT NOT NULL,
+    width INTEGER,
+    height INTEGER,
+    size_kb INTEGER NOT NULL,
+    format TEXT,
+    validation_status TEXT NOT NULL ,
+    notes TEXT,
+    processed_timestamp DATETIME
+
+);
+
+"""
+
+cursor.execute(create_table_sql)
+
+conn.commit()
+conn.close()
+
+print(f"Database {DB_FILE} created successfully!")
